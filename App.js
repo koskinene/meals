7f112eb324b7befb8c84cd97240629c0cebe1480
Erika Koskinen <bgx093@myy.haaga-helia.fi>
2023-09-14 21:58:30 +0300

    Reseptin haku valmis

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Alert, StyleSheet, Text, View, Button, TextInput, FlatList, StatusBar, Image } from 'react-native';
 
 export default function App() {
+  const [keyword, setKeyword] = useState('');
+  const [recipes, setRecipes] = useState([]);
+ 
+  const getRecipes = () => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${keyword}`)
+    .then(response => response.json())
+    .then(responseJson => setRecipes(responseJson.meals))
+    .catch(error => { 
+        Alert.alert('Error', error); 
+    });    
+  }
+  
+  const listSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: "80%",
+          backgroundColor: "#CED0CE",
+          marginLeft: "10%"
+        }}
+      />
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar hidden={true} />
+      <FlatList 
+        style={{marginLeft : "5%"}}
+        keyExtractor={(item, index) => index.toString()} 
+        renderItem={({item}) => 
+          <View>
+            <Text style={{fontSize: 18, fontWeight: "bold"}}>{item.strMeal}</Text>
+            <Image source={{uri: item.strMealThumb+"/preview"}} style={{height: 75, width: 75}}/>
+          </View>}
+        data={recipes} 
+        ItemSeparatorComponent={listSeparator} /> 
+      <TextInput 
+        style={{fontSize: 18, width: 200}} 
+        placeholder='keyword' 
+        value={keyword}
+        onChangeText={text => setKeyword(text)} 
+       />
+      <Button title="Find" onPress={getRecipes} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ container: {
+  flex: 1,
+  backgroundColor: '#fff',
+  alignItems: 'center',
+  justifyContent: 'center',
+  paddingBottom: 50,
+  paddingTop: 100
+ },
 });
